2bc9284b1974ae653b41bc8335c3b9320e7c2f20
MadanB98 <madanbura56@gmail.com>
2024-08-12 19:00:13 +0530

    .

---
+++ src/main/kotlin/ArithmeticOperators/ArithmeticOperators_Example.kt
@@ -0,0 +1,26 @@
+package ArithmeticOperators
+
+fun main() {
+    val a = 20
+    val b = 6
+
+    // Addition
+    val sum = a + b
+    println("Sum: $sum")
+
+    // Subtraction
+    val difference = a - b
+    println("Difference: $difference")
+
+    // Multiplication
+    val product = a * b
+    println("Product: $product")
+
+    // Division
+    val quotient = a / b
+    println("Quotient: $quotient")
+
+    // Modulus (remainder)
+    val remainder = a % b
+    println("Remainder: $remainder")
+}
